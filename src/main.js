4db6acd94490f3a937d009bdca260c21e031aaba
ryuki1126 <uv.ryuki1126@gmail.com>
2023-07-10 21:27:47 +0900

    初期化

--- src/main.js
+++ src/main.js
@@ -1,14 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-// import liff from "@line/liff";
 
 createApp(App).mount('#app')
-
-// async function main() {
-
-//   // VueRouter が動き始める前に liff.init する
-//   await liff.init({ liffId: "1661491250-Rb78d1d6" });
-
-// }
-
-// main();
