1224315b15c631d03fb25cfec4a381972e453f4a
ryuki1126 <uv.ryuki1126@gmail.com>
2023-07-11 14:11:05 +0900

    ログイン状況を確認できるよう修正

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
+  <p v-if="isLogin">ログイン中</p>
+  <p v-if="!isLogin">ログインしていません</p>
 </template>
 
 <script>
@@ -7,10 +9,16 @@ import liff from "@line/liff";
 
 export default {
   name: "App",
+  data() {
+    return {
+      isLogin: false,
+    };
+  },
   created() {
     liff
       .init({ liffId: "2000094702-vgO5A1wk" })
       .then(() => {
+        this.isLogin = liff.isLoggedIn();
         console.log(liff.isLoggedIn());
         console.log(liff.getIDToken());
         console.log(liff.getAccessToken());
